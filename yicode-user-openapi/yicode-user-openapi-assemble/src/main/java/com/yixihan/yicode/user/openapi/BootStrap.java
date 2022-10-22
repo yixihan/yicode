74c27583f456eca73024716837296f5c3a5dc09a
zengsitong <zengsitong@newhope.cn>
2022-10-22 17:17:23 +0800

    初始化user-openapi模块

---
+++ yicode-user-openapi/yicode-user-openapi-assemble/src/main/java/com/yixihan/yicode/user/openapi/BootStrap.java
@@ -0,0 +1,38 @@
+package com.yixihan.yicode.user.openapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+import java.util.Arrays;
+
+/**
+ * user openapi 主启动类
+ *
+ * @author yixihan
+ * @date 2022-10-22-16:52
+ */
+@Slf4j
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableCaching
+@EnableScheduling
+@RefreshScope
+@EnableOpenApi
+@SpringBootApplication
+public class BootStrap {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(BootStrap.class);
+        Environment env = springApplication.run(args).getEnvironment();
+        log.info("openapi modules server has started : {}, CPU core : {}"
+                , Arrays.toString(env.getActiveProfiles()), Runtime.getRuntime().availableProcessors());
+    }
+}
