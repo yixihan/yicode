a58bca4cc22d7d16c41a10d79fac7ee30fece0e6
zengsitong <zengsitong@newhope.cn>
2022-12-19 09:38:24 +0800

    个人网址核心层服务类-实现

--- yicode-user/yicode-user-biz/src/main/java/com/yixihan/yicode/user/biz/service/extra/impl/UserWebsiteServiceImpl.java
+++ yicode-user/yicode-user-biz/src/main/java/com/yixihan/yicode/user/biz/service/extra/impl/UserWebsiteServiceImpl.java
@@ -1,7 +1,12 @@
 package com.yixihan.yicode.user.biz.service.extra.impl;
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yixihan.yicode.common.exception.BizCodeEnum;
 import com.yixihan.yicode.common.reset.dto.responce.CommonDtoResult;
+import com.yixihan.yicode.common.util.CopyUtils;
 import com.yixihan.yicode.user.api.dto.request.extra.ModifyUserWebsiteDtoReq;
 import com.yixihan.yicode.user.api.dto.response.extra.UserWebsiteDtoResult;
 import com.yixihan.yicode.user.biz.service.extra.UserWebsiteService;
@@ -9,7 +14,9 @@ import com.yixihan.yicode.user.dal.mapper.extra.UserWebsiteMapper;
 import com.yixihan.yicode.user.dal.pojo.extra.UserWebsite;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * <p>
@@ -24,21 +31,65 @@ public class UserWebsiteServiceImpl extends ServiceImpl<UserWebsiteMapper, UserW
 
     @Override
     public CommonDtoResult<Boolean> addUserLanguage(ModifyUserWebsiteDtoReq dtoReq) {
-        return null;
+        if (StrUtil.isBlank (dtoReq.getUserWebsite ())) {
+            return new CommonDtoResult<> (Boolean.FALSE, "个人网址信息为空");
+        }
+        
+        UserWebsite website = new UserWebsite ();
+        website.setUserWebsite (dtoReq.getUserWebsite ());
+        website.setUserId (dtoReq.getUserId ());
+    
+        int insert = baseMapper.insert (website);
+        if (insert == 1) {
+            return new CommonDtoResult<> (Boolean.TRUE);
+        } else {
+            return new CommonDtoResult<> (Boolean.FALSE, BizCodeEnum.FAILED_TYPE_BUSINESS.getMsg ());
+        }
     }
 
     @Override
     public CommonDtoResult<Boolean> modifyUserLanguage(ModifyUserWebsiteDtoReq dtoReq) {
-        return null;
+        if (StrUtil.isBlank (dtoReq.getUserWebsite ())) {
+            return new CommonDtoResult<> (Boolean.FALSE, "个人网址信息为空");
+        }
+    
+        UpdateWrapper<UserWebsite> wrapper = new UpdateWrapper<> ();
+        wrapper.eq (UserWebsite.USER_ID, dtoReq.getUserId ())
+                .set (UserWebsite.USER_WEBSITE, dtoReq.getUserWebsite ());
+        int update = baseMapper.update (null, wrapper);
+        if (update == 1) {
+            return new CommonDtoResult<> (Boolean.TRUE);
+        } else {
+            return new CommonDtoResult<> (Boolean.FALSE, BizCodeEnum.FAILED_TYPE_BUSINESS.getMsg ());
+        }
     }
 
     @Override
     public CommonDtoResult<Boolean> delUserLanguage(ModifyUserWebsiteDtoReq dtoReq) {
-        return null;
+        if (StrUtil.isBlank (dtoReq.getUserWebsite ())) {
+            return new CommonDtoResult<> (Boolean.FALSE, "个人网址信息为空");
+        }
+    
+        QueryWrapper<UserWebsite> wrapper = new QueryWrapper<> ();
+        wrapper.eq (UserWebsite.USER_ID, dtoReq.getUserId ())
+                .eq (UserWebsite.USER_WEBSITE, dtoReq.getUserWebsite ());
+    
+        int delete = baseMapper.delete (wrapper);
+        if (delete == 1) {
+            return new CommonDtoResult<> (Boolean.TRUE);
+        } else {
+            return new CommonDtoResult<> (Boolean.FALSE, BizCodeEnum.FAILED_TYPE_BUSINESS.getMsg ());
+        }
     }
 
     @Override
     public List<UserWebsiteDtoResult> getUserLanguage(Long userId) {
-        return null;
+        QueryWrapper<UserWebsite> wrapper = new QueryWrapper<> ();
+        wrapper.eq (UserWebsite.USER_ID, userId);
+    
+        List<UserWebsite> values = Optional.ofNullable (baseMapper.selectList (wrapper))
+                .orElse (Collections.emptyList ());
+        
+        return CopyUtils.copyMulti (UserWebsiteDtoResult.class, values);
     }
 }
