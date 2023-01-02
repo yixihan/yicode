098c9509b71307c37b21e36854e95f46c925d820
zengsitong <zengsitong@newhope.cn>
2023-01-02 13:47:38 +0800

    重构项目架构

---
+++ yicode-question-openapi/yicode-question-openapi-assemble/src/main/java/com/yixihan/yicode/question/openapi/BootStrap.java
@@ -0,0 +1,40 @@
+package com.yixihan.yicode.question.openapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+import java.util.Arrays;
+
+/**
+ * yicode question openapi 主启动类
+ *
+ * @author yixihan
+ * @date 2023/1/2 13:39
+ */
+@Slf4j
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableCaching
+@EnableScheduling
+@RefreshScope
+@EnableOpenApi
+@EnableTransactionManagement
+@SpringBootApplication
+public class BootStrap {
+    
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(BootStrap.class);
+        Environment env = springApplication.run(args).getEnvironment();
+        log.info("question openapi modules server has started : {}, CPU core : {}",
+                Arrays.toString(env.getActiveProfiles()), Runtime.getRuntime().availableProcessors());
+    }
+}
