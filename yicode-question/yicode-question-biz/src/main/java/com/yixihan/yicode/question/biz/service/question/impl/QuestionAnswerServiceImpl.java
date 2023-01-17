bbb4e4924195c321bee7e53c00932d11cdf36f48
zengsitong <zengsitong@newhope.cn>
2023-01-17 16:15:52 +0800

    问题模块 基础模块 问题答案核心层实现

--- yicode-question/yicode-question-biz/src/main/java/com/yixihan/yicode/question/biz/service/question/impl/QuestionAnswerServiceImpl.java
+++ yicode-question/yicode-question-biz/src/main/java/com/yixihan/yicode/question/biz/service/question/impl/QuestionAnswerServiceImpl.java
@@ -1,8 +1,14 @@
 package com.yixihan.yicode.question.biz.service.question.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollectionUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yixihan.yicode.common.exception.BizCodeEnum;
 import com.yixihan.yicode.common.reset.dto.responce.CommonDtoResult;
 import com.yixihan.yicode.common.reset.dto.responce.PageDtoResult;
+import com.yixihan.yicode.common.util.PageUtil;
 import com.yixihan.yicode.question.api.dto.request.question.AddQuestionAnswerDtoReq;
 import com.yixihan.yicode.question.api.dto.request.question.QueryQuestionAnswerDtoReq;
 import com.yixihan.yicode.question.api.dto.response.question.QuestionAnswerDtoResult;
@@ -10,6 +16,8 @@ import com.yixihan.yicode.question.biz.service.question.QuestionAnswerService;
 import com.yixihan.yicode.question.dal.mapper.question.QuestionAnswerMapper;
 import com.yixihan.yicode.question.dal.pojo.question.QuestionAnswer;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * <p>
@@ -24,16 +32,41 @@ public class QuestionAnswerServiceImpl extends ServiceImpl<QuestionAnswerMapper,
     
     @Override
     public CommonDtoResult<Boolean> addQuestionAnswer(AddQuestionAnswerDtoReq dtoReq) {
-        return null;
+        QuestionAnswer questionAnswer = BeanUtil.toBean (dtoReq, QuestionAnswer.class);
+    
+        int modify = baseMapper.insert (questionAnswer);
+        
+        if (modify != 1) {
+            return new CommonDtoResult<> (Boolean.FALSE, BizCodeEnum.FAILED_TYPE_BUSINESS.getErrorMsg ());
+        }
+        return new CommonDtoResult<> (Boolean.TRUE);
     }
     
     @Override
     public QuestionAnswerDtoResult questionAnswerDetail(Long questionAnswerId) {
-        return null;
+        QuestionAnswer questionAnswer = baseMapper.selectById (questionAnswerId);
+        
+        questionAnswer = Optional.ofNullable (questionAnswer).orElse (new QuestionAnswer ());
+        
+        return BeanUtil.toBean (questionAnswer, QuestionAnswerDtoResult.class);
     }
     
     @Override
     public PageDtoResult<QuestionAnswerDtoResult> queryQuestionAnswer(QueryQuestionAnswerDtoReq dtoReq) {
-        return null;
+        Page<QuestionAnswer> pages = baseMapper.selectPage (
+                new Page<> (dtoReq.getPage (), dtoReq.getPageSize (), dtoReq.getSearchCount ()),
+                new QueryWrapper<QuestionAnswer> ()
+                        .eq (QuestionAnswer.QUESTION_ID, dtoReq.getQuestionId ())
+                        .eq (QuestionAnswer.USER_ID, dtoReq.getUserId ())
+        );
+        
+        if (CollectionUtil.isEmpty (pages.getRecords ())) {
+            return new PageDtoResult<> ();
+        }
+        
+        return PageUtil.pageToPageDtoResult (
+                pages,
+                (o) -> BeanUtil.toBean (o, QuestionAnswerDtoResult.class)
+        );
     }
 }
