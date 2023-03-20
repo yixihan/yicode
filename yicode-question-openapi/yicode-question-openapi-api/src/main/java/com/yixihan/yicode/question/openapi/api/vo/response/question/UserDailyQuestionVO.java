9603b63d94228da0f5f35b8cac2d05ff468cdda9
yixihan <yixihan20010617@gmail.com>
2023-03-20 23:30:48 +0800

    fix 每日一题模块 获取用户每日一题做题情况

--- yicode-question-openapi/yicode-question-openapi-api/src/main/java/com/yixihan/yicode/question/openapi/api/vo/response/question/UserDailyQuestionVO.java
+++ yicode-question-openapi/yicode-question-openapi-api/src/main/java/com/yixihan/yicode/question/openapi/api/vo/response/question/UserDailyQuestionVO.java
@@ -31,6 +31,6 @@ public class UserDailyQuestionVO {
     private Long userId;
     
     @ApiModelProperty(value = "创建时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date createTime;
 }
